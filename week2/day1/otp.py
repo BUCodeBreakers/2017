ae7638a1a1950582a2f3b2a7873aed45a6cc0171
Sarah Scheffler <sscheffler@hmc.edu>
2016-07-13 12:43:17 -0400

    Adding otp.py for week 2 day 1

---
+++ week2/day1/otp.py
@@ -0,0 +1,26 @@
+import random
+
+def getKey(n):
+	"""Generates a random key to encrypt an n-length string"""
+	#TODO
+	return
+
+def stringToAscii(string):
+	"""Encode a string to its ASCII encoding"""
+	#TODO
+	return
+
+def asciiToString(ascii):
+	"""Decode ASCII encoding to a string"""
+	#TODO
+	return
+
+def encrypt(message, key):
+	"""Return a ciphertext string based on the message"""
+	#TODO
+	return
+
+def decrypt(ciphertext, key):
+	"""Return a plaintext message string based on the ciphertext and key"""
+	#TODO
+	return
