0dfcb510a543b6242bc7dd269a12e0676286f890
BUCodeBreakers <cyber@bu.edu>
2016-07-18 08:52:37 -0400

    Add files via upload

---
+++ Exercise_1_Basic_Python.py
@@ -0,0 +1,99 @@
+
+# coding: utf-8
+
+# In[2]:
+
+#Name:
+#Assignment: 1
+#Date: 
+#Collaborators:
+#Exercise 1: Basic Python
+
+
+##########################################
+# (A) Comments
+##########################################
+
+#This is a comment. Python will ignore it when you run the file.
+
+"""This is also a comment. Made by 3 sets of double quotation marks."""
+
+#Write your last name as a comment here:
+
+
+#Now print your first name:
+
+
+##########################################
+# (B) Variables and Casting
+##########################################
+
+#Save your first name to a variable named "first"
+
+#Save your last name to a variable named "last"
+
+#Print out your full name by adding a space between both variables
+
+
+##########################################
+# Mathematical Operations
+##########################################
+
+#Choose a random number and assign it to a variable
+
+#Assign double of the number to another variable (through multiplication of the first variable)
+
+#Add 6
+
+#Divide it in half
+
+#Subtract the original number from the one you have now
+
+#Print what you have left
+
+
+##########################################
+# Bonus: The Four Fours Challenge
+##########################################
+
+#Using the following operations only:
+#  +    addition
+#  -    subtraction or negation
+#  *    multiplication
+#  /    division
+#  (   )    parentheses for grouping
+#  **    power
+
+#Your task: compute the following values using exactly four 4's
+#You may also use 44 and 4.4 where each is considered to be two 4's
+
+#Examples for zero:
+zero =  4 / 4 * 4 - 4
+zero2 = 44 - 44 
+#One
+
+#Two
+
+#Three
+
+#Four
+
+#Five
+
+#Six
+    
+    
+##########################################
+# Bonus 2: More Four Fours
+##########################################
+
+#Seven
+
+#Eight
+
+#Nine
+
+#Ten
+
+
+
---
+++ Exercise_2_For_Loops.py
@@ -0,0 +1,75 @@
+
+# coding: utf-8
+
+# In[8]:
+
+#Name:
+#Exercise: 3
+#Date: 
+#Collaborators:
+#Exercise_2_While_Loops
+
+##########################################
+# (A)
+##########################################
+
+#Use len() to find the length of the following strings and print it
+
+string1 = "cryptography"
+string2 = "Am I doing this right?"
+string3 = "whatAboutThis?"
+string4 = "BuCodeBreakers2016!!!"
+
+
+#Does the length function ignore or count spaces?
+#Answer: 
+
+#Does length work on integers? Try it out below to see.
+#Answer:
+
+
+##########################################
+# (B)
+##########################################
+
+#Write a for loop that prints out each individual letter of the following string.
+#Remember to use len() to find the length of the string
+
+word = "cryptography"
+
+
+
+
+##########################################
+# (C)
+##########################################
+
+#Write a Python program that reverses the following word. Print the whole reversed word together.
+word2 = "cybersecurity"
+
+
+
+##########################################
+# Bonus
+##########################################
+
+#Try to do (B) in 2 lines
+
+
+
+
+##########################################
+# Bonus 2
+##########################################
+
+#Using one for loop, add the two strings together to get one string of the alphabet in order
+word3 = "acegikmoqsuwy"
+word4 = "bdfhjlnprtvxz"
+
+
+
+
+# In[ ]:
+
+
+
