9c4450444b0bd15050db05756968229d6d7014d3
BUCodeBreakers <cyber@bu.edu>
2016-07-19 14:28:59 -0400

    Add files via upload

---
+++ Exercise_3.py
@@ -0,0 +1,132 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#Name:
+#Exercise: 3
+#Date: 
+#Collaborators:
+#Exercise_3_Functions
+#Booleans + Conditionals + Functions
+
+##########################################
+# (A)
+##########################################
+
+#Write a function, printWorld() that takes no inputs and prints “Hello World!”
+
+#first define the function, remember to put empty brackets when there are no inputs
+
+
+#now call the function
+    
+
+##########################################
+# (B)
+##########################################
+
+#Now write a function, multiply_by_2(n), that takes one integer input, multiplies it by 2 and prints the result
+
+#function here
+
+
+print("")
+print("Multiply by 2")
+multiply_by_2(2)
+multiply_by_2(16)
+multiply_by_2(225)
+multiply_by_2(12143)
+print("Answers should be: 4, 32, 450, 24286")
+
+##########################################
+# (C)
+##########################################
+
+#Now write a function, multiply_by_3(n), that takes one integer input, multiplies it by 2 and RETURNS the result
+#print the result
+
+
+#function here
+
+print("")
+print("Multiply by 3")
+print(multiply_by_3(3) == 9)
+print(multiply_by_3(5) == 15)
+print(multiply_by_3(125) == 375)
+print(multiply_by_3(102934) == 308802)
+print("All should be True")
+
+
+
+
+##########################################
+# (D)
+##########################################
+
+num = 5
+#Use the previous 2 functions to multiply num by 36
+#Do not use the asterisk (*) operator 
+
+
+
+
+##########################################
+# (E)
+##########################################
+
+#Write a function, multiply(n, m), that takes two integer inputs, n and m.
+#Take n and multiply by m
+#return the result
+#print what the function returns 
+
+
+
+##########################################
+# (F)
+##########################################
+
+#Write a Python program, equal_to_five(n), that accepts an integer n, and returns True if it is equal to 5
+#Return False, if not equal
+
+
+
+##########################################
+# Bonus 1
+##########################################
+
+#Write a Python function to find the Max of three numbers
+#Remember to use if statements and == to compare values
+
+def max_of_3(a,b,c):
+    
+
+
+
+
+
+##########################################
+# Bonus 2
+##########################################
+
+#Write a Python program, compare(n,m), that accepts an integer n
+#if n is less than m, return  -  n "is less than" m
+#if n is equal to than m, return  -  n "is equal to" m
+#if n is more than m, return  -  n "is more than" m
+#Be sure to use if, elif and else along with the proper boolean functions
+
+
+
+
+
+
+##########################################
+# Bonus 2
+##########################################
+
+
+
+
+
+
+
