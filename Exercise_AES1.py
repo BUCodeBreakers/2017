740a4113e01d2ad477c87a4769afbd4c3b43fcd9
BUCodeBreakers <cyber@bu.edu>
2016-07-26 13:11:59 -0400

    Creating AES confidentiality exercise

---
+++ Exercise_AES1.py
@@ -0,0 +1,32 @@
+#Part 1: AES Confidentiality
+
+# In your Ubuntu VM, load "Terminal".  Type in:
+python3
+
+# Enter the following into the shell (terminal):
+
+from Crypto.Cipher import AES
+import os
+
+# Come up with a 16-character key and share it with someone.
+key = 'ilikethiskey1234' # change this!
+
+# Come up with a message that is as long as a multiple of 16 (16 characters,
+# 32 characters, 48 characters....)
+# DO NOT share the message with your friend!
+message1 = 'sixteen-char msg'
+message2 = 'this message has 0032 characters'
+
+# Define a cipher object by doing this:
+cipher = AES.new(key, AES.MODE_CBC, os.urandom(16))
+
+# Calculate the ciphertext and share it with the friend you gave the key.
+ciphertext1 = cipher.encrypt(message)
+print(ciphertext1)
+
+# Have your friend give you a new ciphertext called ciphertext2, encrypted with
+# the same key.
+ciphertext2 = #whatever your friend gives you...
+
+# Then decrypt it!
+cipher.decrypt(ciphertext2)
