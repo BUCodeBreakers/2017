3a1a90db69b341dba7649fb67146722de40215ce
BUCodeBreakers <cyber@bu.edu>
2017-07-17 14:52:37 -0400

    week2_day1

---
+++ caesarDe.py
@@ -0,0 +1,16 @@
+
+
+
+print("Welcome to Caesar Decryption")
+encrypted = input('Enter Your Encrypted Message: ')
+key = input('Enter Your Key: ')
+
+#Your code here
+message = ''
+
+
+print ("Your Message is: " + message)
+
+
+
+# The code is very similar to the encryption part of it 
---
+++ caesarEn.py
@@ -0,0 +1,23 @@
+
+print ("Welcome to Caesar Encryption")
+message = input('Enter Your Message: ')
+key = input('Enter Your Key: ')
+encrypted = '' 
+
+#your code here
+
+
+print ("Your Encrypted Message is: " + encrypted)
+
+# Bonus: handles when the input is not letters
+
+# What is the command that you use to display message
+
+# What is the command that you use to store input
+
+# create a variable that stores your encrypted message
+
+# how do you LOOP through all characters in message? 
+# HINTS: what is the ASCII for the key and each of the characters in the message. 
+#        Try to make the A correspond to 0, etc. 
+#        what is the formula for caesar cipher encryption?
