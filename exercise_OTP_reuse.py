ba5fd2b9c03c2010ca7ed86d968df7446ce6bfdd
Sarah Scheffler <sarahscheffler@users.noreply.github.com>
2017-07-20 10:38:03 -0400

    Create exercise_OTP_reuse.py

---
+++ exercise_OTP_reuse.py
@@ -0,0 +1,6 @@
+# These are each a single, lower-case English word encrypted under
+# One Time Pad with the same 11-byte key.  Figure out what the original
+# English words were.
+
+ciphertext1 = [68, 1, 45, 64, 219, 164, 162, 123, 55, 90, 19]
+ciphertext2 = [72, 6, 57, 65, 193, 161, 170, 108, 63, 65, 24]
