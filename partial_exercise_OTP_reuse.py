e4fa58e6aaf66b8262a1d2fc2d2bfc4487b3241d
BUCodeBreakers <sscheffler@hmc.edu>
2017-07-20 13:03:33 -0400

    reduced complexity in otp exercise

--- partial_exercise_OTP_reuse.py
+++ partial_exercise_OTP_reuse.py
@@ -32,28 +32,7 @@ pp.pprint(letter_pairs)
 with open("11letterwords.txt", "r") as f:
     word_list = f.read().split()
 
-# 3.1: Reduce search space.  Figure out which words work AT ALL.
-reduced_word_list = []
-
-# 3.1.1: Build list of letters that can go in this spot *at all*
-valid_letters = [] # build this in a for loop!
-
-
-# 3.1.2: Build function to test whether a word is allowed or not
-def word_is_okay(word):
-    # change this so that it tests whether the word can be built with valid_letters
-    return True
-
-# 3.1.3: Loop over words, only keep words that are allowed
-for word in word_list:
-    # change this so it does something!
-    []
-
-# You should have 1354 options left at this point (reduced_word_list should be
-# 1354 long)
-
-
-# 3.2: Now do pair checking.  For each word, create its pair word, see if it's
+# 3.1: Do pair checking.  For each word, create its pair word, see if it's
 # in the word list.
 
 # 3.2.1: Write a function to compute the paired word, assuming every letter in
@@ -63,11 +42,8 @@ def get_pair(word):
     return ""
 
 # 3.2.2: Loop over the words and see if any of them have a valid pair
-for word in reduced_word_list:
+for word in word_list:
     # change this so it checks if the pair word is in the list
     []
-
-
-
         
 
